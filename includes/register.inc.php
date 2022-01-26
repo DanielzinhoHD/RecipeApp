72a2e34385bc44dad65828fa71f83a809476fab5
Danielzinho <christiandanielgomes@gmail.com>
2022-01-26 15:56:19 -0300

    sendgrid sendemail upload

--- includes/register.inc.php
+++ includes/register.inc.php
@@ -10,14 +10,15 @@ if(isset($_POST["submit"])){
 
     require_once 'dbh.inc.php';
     require_once 'functions.inc.php';
-
-    if(toShortName($name) !== false){
-        echo "<p class='error'>This name is to short!</p>";
-        exit();
-    }
+    require_once 'sendemail.inc.php';
 
     if(emptyInputsRegister($name, $email, $password, $password2) !== false){
         echo "<p class='error'>You need to fill all the inputs!</p>";
+        exit();
+    }
+
+    if(toShortName($name) !== false){
+        echo "<p class='error'>This name is to short!</p>";
         exit();
     }
 
@@ -40,15 +41,17 @@ if(isset($_POST["submit"])){
     // Send email;
         $to = $email;
         $subject = "Random Recipes - Email Verification";
-        $message = "<p>Click the link below to verify your email:</p>
+        $content = "<p>Click the link below to verify your email:</p>
         <a href='https://recipeapp-exercise.herokuapp.com/includes/verify.inc.php?vkey=$vkey'>
             Register account
         </a>";
-        $headers = "From: recipeappexercise@gmail.com" . "\r\n";
-        $headers .= "MIME-Version: 1.0" . "\r\n";
-        $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+        // $headers = "From: recipeappexercise@gmail.com" . "\r\n";
+        // $headers .= "MIME-Version: 1.0" . "\r\n";
+        // $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+
+        SendEmail::sendMail($to, $subject, $content);
         
-        mail($to, $subject, $message, $headers);
+        // mail($to, $subject, $message, $headers);
         exit();
     }else{
         exit();
---
+++ includes/sendemail.inc.php
@@ -0,0 +1,26 @@
+<?php
+
+require "../vendor/autoload.php";
+
+class SendEmail{
+
+    public static function sendMail($to, $subject, $content){
+        $key = parse_url(getenv("SENDGRID_API_KEY"));
+
+        $email = new \SendGrid\Mail\Mail();
+        $email->setFrom("randomrecipes@gmail.com", "Random Recipes");
+        $email->setSubject($subject);
+        $email->addTo($to);
+        $email->addContent("text/html", $content);
+
+        $sendGrid = new \SendGrid($key);
+        
+        try{
+            $response = $sendGrid->send($email);
+            return $response;
+        }catch(Exception $e){
+            echo 'Caught email exception: '.$e->getMessage()."\n";
+            return false;
+        }
+    }
+}
